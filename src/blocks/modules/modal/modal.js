c30d6c8aa4da84eb2b3d0625a10c238c26f10177
Ilia Shibaev <hausdr@gmail.com>
2025-10-31 18:12:52 +0300

    fix: fix styles

--- src/blocks/modules/modal/modal.js
+++ src/blocks/modules/modal/modal.js
@@ -1,32 +1,15 @@
+let currentModal = null;
+const body = document.body;
+
 function initModals() {
-    const body = document.body;
     const modals = document.querySelectorAll(".js-modal");
-    let currentModal = null;
 
     document.querySelectorAll(".js-open-modal:not(.modal-init)").forEach(btn => {
         btn.classList.add("modal-init");
         btn.addEventListener("click", e => {
-            const modal = document.querySelector(`#${e.currentTarget.dataset.modalId}`);
-            if (!modal) return;
-
-            currentModal = modal;
-            modal.classList.add("modal_open");
-            body.classList.add("modal-open");
-
-            modal.querySelector(".js-button-close")?.addEventListener("click", closeModal);
+            openModal(e.currentTarget.dataset.modalId);
         });
     });
-
-    function closeModal(e) {
-        e?.stopPropagation();
-
-        if (currentModal) {
-            body.classList.remove("modal-open");
-            currentModal.classList.remove("modal_open");
-            currentModal.querySelector(".js-button-close")?.removeEventListener("click", closeModal);
-            currentModal = null;
-        }
-    }
 
     modals.forEach(modal => {
         modal.addEventListener("click", e => e.target === modal && closeModal());
@@ -34,5 +17,55 @@ function initModals() {
 
     document.addEventListener("keydown", e => e.key === "Escape" && closeModal());
 }
+
+export function openModal(modalId, response = "", type = "success") {
+    const modal = document.querySelector(`#${modalId}`);
+    if (!modal) return;
+
+    currentModal = modal;
+    modal.classList.add("modal_open");
+    body.classList.add("modal-open");
+
+    modal.querySelector(".js-button-close")?.addEventListener("click", closeModal);
+
+    if (response) {
+        modal.querySelector(".js-modal-text").textContent = response;
+        modal.classList.add(`modal_${type}`);
+    }
+
+}
+
+export function closeModal(e) {
+    e?.stopPropagation();
+
+    if (currentModal) {
+        body.classList.remove("modal-open");
+        currentModal.classList.remove("modal_open");
+        currentModal.querySelector(".js-button-close")?.removeEventListener("click", closeModal);
+        currentModal = null;
+    }
+}
+
+document.addEventListener("wpcf7mailsent", function(event) {
+    const response = event.detail.apiResponse.message;
+    openModal("popup", response, "success");
+
+    setTimeout(() => {
+        if (currentModal) {
+            closeModal();
+        }
+    }, 5000);
+}, false);
+
+document.addEventListener("wpcf7mailfailed", function(event) {
+    const response = event.detail.apiResponse.message;
+    openModal("popup", response, "error");
+
+    setTimeout(() => {
+        if (currentModal) {
+            closeModal();
+        }
+    }, 5000);
+}, false);
 
 initModals();
