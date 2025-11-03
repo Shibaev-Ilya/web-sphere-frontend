ce68bb004b314dafacea33c2003aed07c6b75c64
AlexDrags <ksander-88@yandex.ru>
2025-11-03 17:26:39 +0300

    feat:add animation to marquee

--- src/app/components/Marquee/Marquee.tsx
+++ src/app/components/Marquee/Marquee.tsx
@@ -1,17 +1,32 @@
+'use client';
+import { useEffect } from 'react';
 import './marquee.scss';
 import MarqueeEl from "app/ui/marqueeEl/marqueeEl";
 export default function Marquee() {
-    const countEl = new Array(2);
-    console.log(countEl)
+    const countEl = ['', ''];
+    useEffect(() => {
+        const observer = new IntersectionObserver((entries) => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add("visible");
+            }
+        });
+        }, {
+            rootMargin: "0px 0px -70px 0px" // Элемент появится когда до него останется 50px
+        });
+        const marqueeTarget = document.querySelector(".marquee");
+        observer.observe(marqueeTarget);
+    }, []);
+
     return (
         <section className="marquee js-scroll-animate scroll-animate">
             <div className="container">
                 {
-                    countEl.map(() => {
+                    countEl.map((s, i) => {
                         return (
-                            <div className="marquee__inner">
-                                <MarqueeEl />
-                                <MarqueeEl />
+                            <div className="marquee__inner" key={i}>
+                                {i == 1 ? <MarqueeEl /> : <MarqueeEl reverse={'marquee__items_reverse'} />}
+                                {i == 1 ? <MarqueeEl /> : <MarqueeEl reverse={'marquee__items_reverse'} />}
                             </div>
                         )
                     })
--- src/app/ui/marqueeEl/marqueeEl.tsx
+++ src/app/ui/marqueeEl/marqueeEl.tsx
@@ -1,6 +1,6 @@
-export default function MarqueeEl() {
+export default function MarqueeEl({reverse}) {
     return (
-        <div className="marquee__items">
+        <div className={`marquee__items ${reverse ? `${reverse}` : ''}`}>
                 <div className="marquee__item">Get best offer!</div>
                 <div className="marquee__item">Web Sphere</div>
                 <div className="marquee__item">Get best offer!</div>
